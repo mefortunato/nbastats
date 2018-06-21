f5cbe71d1da024e40ff623d2069bff9d88a53baf
Mike Fortunato <mef231@gmail.com>
2018-06-21 15:06:48 +0000

    1983 and 1984 had missing data; fixed bug when sorting by game id instead of game date

--- test/test_expanding_average.py
+++ test/test_expanding_average.py
@@ -3,31 +3,29 @@ import pandas as pd
 
 import unittest
 
+def check_avg(df_, season, team, stats):
+    avg_stats = ['AVG_'+stat for stat in stats]
+    df = df_.loc[(df_['SEASON_ID'] == season)& (df_['TEAM_ABBREVIATION'] == team)].sort_values('GAME_DATE')
+    avg = df[stats].expanding().mean().shift(1)
+    # use index [1:] because first value is na
+    diff = np.abs(df[avg_stats].values[1:] - avg[stats].values[1:])
+    by_stat = np.all(diff<1e-4, axis=0)
+    print(season, team, np.argwhere(by_stat == False))
+    return(np.all(diff<1e-4))
+
 class TestExpandingAverage(unittest.TestCase):
     def setUp(self):
         self.data = pd.read_csv('../dat/expanding_avg_1983-2017.csv', index_col=0).sort_values(['SEASON_ID','TEAM_ID', 'GAME_DATE'])
         
-    def test_expanding_averages(self):
-        STATS = ['PTS', 'AST', 'DREB']
-        AVG_STATS = ['AVG_PTS', 'AVG_AST', 'AVG_DREB']
-        seasons = self.data['SEASON_ID'].unique()
-        for season in seasons:
-            if season == 21984:
-                continue
-            s_df = self.data.loc[self.data['SEASON_ID'] == season]
-            teams = s_df['TEAM_ID'].unique()
-            for team in teams:
-                t_df = s_df.loc[s_df['TEAM_ID'] == team].sort_values(['SEASON_ID','TEAM_ID', 'GAME_DATE'])
-                self.assertTrue(np.all(t_df.head(1)[AVG_STATS].isna(), axis=1), 'first game has averages')
-                correct = t_df[STATS].expanding().mean().shift(1).iloc[1:]
-                t_df = t_df.iloc[1:]
-                diff = np.abs(correct[STATS].values-t_df[AVG_STATS].values) < 1e-4
-                #self.assertTrue(np.all(diff), pd.concat([t_df[AVG_STATS], correct[STATS]], axis=1))
-                self.assertTrue(np.all(diff), 'error in season {} team {}'.format(season, team))
-                print('passed season {} team {}'.format(season, team))
-        #phi_2017 = self.data.loc[(self.data['SEASON_ID'] == 22017) & (self.data['TEAM_ABBREVIATION'] == 'PHI')].fillna(0)
-        #correct = phi_2017['PTS'].expanding().mean().shift(1).fillna(0)
-        #self.assertTrue(np.all(np.abs(correct - phi_2017['AVG_PTS']) < 1e-4))
+    def test_expanding_stat(self):
+        stats = ['PTS', 'FG2M', 'FG2A', 'FG2_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'DREB', 'OREB', 'AST', 'STL', 'BLK', 'TOV', 'PF']
+        avg_stats = ['AVG_'+stat for stat in stats]
+        for season in self.data['SEASON_ID'].unique():
+            for team in self.data.loc[self.data['SEASON_ID']==season]['TEAM_ABBREVIATION'].unique():
+                test_df = self.data.loc[(self.data['SEASON_ID']==season) & (self.data['TEAM_ABBREVIATION']==team)]
+                test_df = test_df.sort_values('GAME_DATE')
+                self.assertTrue(test_df.iloc[0][avg_stats].isna().all(), 'error in season {} team {}: first game has averages')
+                self.assertTrue(check_avg(self.data, season, team, stats), 'error in season {} team {}: expanding averages inaccurate'.format(season, team))
         
 if __name__ == '__main__':
     unittest.main()
